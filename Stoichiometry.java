ea58775dbfae2a16170fcf6758d0cda05401ab16
NinjaDoggy <ninjadoggy1234@gmail.com>
2017-02-05 02:59:47 -0800

    Add files via upload

--- Stoichiometry.java
+++ Stoichiometry.java
@@ -5,35 +5,35 @@ public class Stoichiometry {
 	public Stoichiometry(Equation equation) {
 		this.equation = equation;
 	}
-	public double evaluate(Chemical given, String startType, double value, Chemical target, String endType) {
+	public String evaluate(Chemical given, String startType, Number value, Chemical target, String endType) {
 		String work = "https://latex.codecogs.com/gif.latex?";
-		work += given.toLatexString(value, getUnit(startType));
-		double startVal = value;
+		work += given.toLatexString(value.toString(), getUnit(startType));
+		double startVal = value.getValue();
 		if (startType.equals("mass")) {
 			startVal /= given.molarMass;
-			work+="\\cdot\\frac{" + given.toLatexString(1, "mol") + "}{" + given.toLatexString(given.molarMass, "g") + "}\\cdot";
+			work+="\\cdot\\frac{" + given.toLatexString("1", "mol") + "}{" + given.toLatexString(""+given.molarMass, "g") + "}\\cdot";
 		}
 		else if (startType.equals("volume")) {
 			startVal /= 22.4;
-			work+="\\frac{" + given.toLatexString(1, "mol") + "}{" + given.toLatexString(22.4, "L") + "}\\cdot";
+			work+="\\frac{" + given.toLatexString("1", "mol") + "}{" + given.toLatexString("22.4", "L") + "}\\cdot";
 		}
 		
-		work+="\\frac{" + target.toLatexString(equation.amountOf(target), "mol")
-			  + "}{" + given.toLatexString(equation.amountOf(given), "L") + "}\\cdot";
+		work+="\\frac{" + target.toLatexString(""+equation.amountOf(target), "mol")
+			  + "}{" + given.toLatexString(""+equation.amountOf(given), "mol") + "}\\cdot";
 		
 		double endVal = 1;
 		if (endType.equals("mass")) {
 			endVal *= target.molarMass;
-			work += "\\frac{" + target.toLatexString(target.molarMass, "g") + "}{" + target.toLatexString(1, "mol") + "}";
+			work += "\\frac{" + target.toLatexString(""+target.molarMass, "g") + "}{" + target.toLatexString("1", "mol") + "}";
 		}
 		else if (endType.equals("volume")) {
 			endVal *= 22.4;
-			work += "\\frac{" + target.toLatexString(22.4, "L") + "}{" + target.toLatexString(1, "mol") + "}";
+			work += "\\frac{" + target.toLatexString("22.4", "L") + "}{" + target.toLatexString("1", "mol") + "}";
 		}
 		double ans = startVal * equation.amountOf(target) / equation.amountOf(given) * endVal;
-		work += "=" + ans;
+		work += "=" + target.toLatexString(Number.numToString(ans, value.getSigFigs()),getUnit(endType));
 		System.out.println(work);
-		return ans;
+		return work;
 	}
 	public String getUnit(String type) {
 		if (type.equals("mass")) {
