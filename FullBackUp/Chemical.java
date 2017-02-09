dcbd154bd39548798568fe72d96a35d1ea27b533
NinjaDoggy <ninjadoggy1234@gmail.com>
2017-02-08 20:32:50 -0800

    Update Chemical.java

--- FullBackUp/Chemical.java
+++ FullBackUp/Chemical.java
@@ -1,99 +1,176 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Chemical implements ChemicalCompoundable{
-	
-	private ChemicalCompoundable[] parts;
-	private ArrayList<Element> rawElements;
-	private String name;
-	private int amount;
-	private String phase;
-	private double molarMass;
+public class Chemical implements ChemicalCompoundable {
+
+	public ChemicalCompoundable[] parts;
+	public ArrayList<Element> rawElements;
+	public String name;
+	public int amount;
+	public String phase;
+	public double molarMass;
 
 	public Chemical(String formula) {
-		this(formula, 1,"Unknown");
+		this(formula, 1, "Unknown");
 	}
-	public Chemical(String formula,int amount){
-		this(formula,amount,"Unknown");
+
+	public Chemical(String formula, int amount) {
+		this(formula, amount, "Unknown");
 	}
-	public Chemical(String formula, int amount, String name) {
+
+	public Chemical(String str, int amount, String name) {
+		if (!isFormula(str)) {
+			System.out.println("detected it's a name");
+			try {
+				URL url;
+				url = new URL("https://www.wolframcloud.com/objects/4e38e001-a051-4752-a78f-390ca9521804");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				String data = URLEncoder.encode("chemical", "UTF-8") + "=" + toWolfram(str);
+				conn.setDoOutput(true);
+				conn.setDoInput(true);
+				conn.setRequestMethod("POST");
+				conn.connect();
+				OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+				wr.write(data);
+				wr.flush();
+
+				if (conn.getResponseCode() > 200) {
+					BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+					while (rd.ready()) {
+						System.out.println(rd.readLine());
+					}
+				} else {
+					BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					str = rd.readLine();
+					str = str.substring(1,str.length()-1);
+					wr.close();
+					rd.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Formula name: "+str);
 		this.name = name;
 		this.amount = amount;
-		ArrayList<String> split = splitString(formula);
-		System.out.println(split);
+		ArrayList<String> split = splitString(str);
 		int numObj = split.size();
 		parts = new ChemicalCompoundable[numObj];
-		for(int i = 0;i<numObj;i++){
+		for (int i = 0; i < numObj; i++) {
 			String cur = split.get(i);
 			String num = "";
-			for(int j = cur.length()-1;;j--){
-				if(Character.isDigit(cur.charAt(j))){
-					num = cur.charAt(j)+num;
-				}else{
+			for (int j = cur.length() - 1;; j--) {
+				if (Character.isDigit(cur.charAt(j))) {
+					num = cur.charAt(j) + num;
+				} else {
 					break;
 				}
 			}
-			int curAmount = num.isEmpty()?1:Integer.parseInt(num);
-			if(cur.charAt(0)=='('){
-				parts[i] = new Chemical(cur.substring(1, cur.indexOf(')')),curAmount);
-			}else{
-				String elem = ""+cur.charAt(0);
-				if(cur.length()>1&&Character.isLetter(cur.charAt(1))){
-					elem+=cur.charAt(1);
+			int curAmount = num.isEmpty() ? 1 : Integer.parseInt(num);
+			if (cur.charAt(0) == '(') {
+				parts[i] = new Chemical(cur.substring(1, cur.indexOf(')')), curAmount);
+			} else {
+				String elem = "" + cur.charAt(0);
+				if (cur.length() > 1 && Character.isLetter(cur.charAt(1))) {
+					elem += cur.charAt(1);
 				}
-				parts[i] = new Element(getAtomicNumber(elem),curAmount);
+				parts[i] = new Element(getAtomicNumber(elem), curAmount);
 			}
 		}
-		//calc molarmass and rawElements
+		// calc molarmass and rawElements
 		molarMass = 0;
 		rawElements = new ArrayList<Element>();
-		for(int i = 0;i<parts.length;i++){
-			molarMass+=parts[i].getMolarMass();
+		for (int i = 0; i < parts.length; i++) {
+			molarMass += parts[i].getMolarMass();
 			addOnElements(parts[i].getElements());
 		}
-		
+
 	}
-	private void addOnElements(ArrayList<Element> arr){
-		for(int i = 0,ii=arr.size();i<ii;i++){
+
+	public static boolean isFormula(String str) {
+		String temp = "";
+		for (int i = 0; i < str.length(); i++) {
+			char cur = str.charAt(i);
+			if(Character.isDigit(cur)){
+				return true;
+			}
+			if(Character.isUpperCase(cur)){
+				if(temp.length()>2||(!temp.isEmpty()&&getAtomicNumber(temp)==-1))return false;
+				temp=""+cur;
+			}else{
+				temp+=cur;
+			}
+		}
+		if(temp.length()>2||getAtomicNumber(temp)==-1)return false;
+		return true;
+	}
+
+	private String toWolfram(String str) {
+		StringTokenizer st = new StringTokenizer(str);
+		String ans = "";
+		while (st.hasMoreTokens()) {
+			String temp = st.nextToken();
+			temp.toLowerCase();
+			char[] arr = temp.toCharArray();
+			arr[0] = Character.toUpperCase(arr[0]);
+			ans += new String(arr);
+		}
+		return ans;
+	}
+
+	private void addOnElements(ArrayList<Element> arr) {
+		for (int i = 0, ii = arr.size(); i < ii; i++) {
 			Element toBeAdded = arr.get(i);
 			int index = rawElements.indexOf(toBeAdded);
-			if(index==-1){
+			if (index == -1) {
 				rawElements.add(new Element(toBeAdded));
-			}else{
-				rawElements.get(index).amount+=toBeAdded.amount;
+			} else {
+				rawElements.get(index).amount += toBeAdded.amount;
 			}
 		}
 	}
-	private ArrayList<String> splitString(String str){
+
+	private ArrayList<String> splitString(String str) {
 		ArrayList<String> arr = new ArrayList<String>();
-		String temp="";
+		String temp = "";
 		boolean chem = false;
-		for(int i = 0;i<str.length();i++){
+		for (int i = 0; i < str.length(); i++) {
 			char cur = str.charAt(i);
-			if(cur==' ')continue;
-			if(chem){
-				if(cur==')'){
+			if (cur == ' ')
+				continue;
+			if (chem) {
+				if (cur == ')') {
 					chem = false;
 				}
-				temp+=cur;
-			}else{
-				if(cur=='('){
+				temp += cur;
+			} else {
+				if (cur == '(') {
 					chem = true;
 				}
-				if(!temp.isEmpty()&&(cur=='('||Character.isUpperCase(cur))){
+				if (!temp.isEmpty() && (cur == '(' || Character.isUpperCase(cur))) {
 					arr.add(temp);
-					temp = ""+cur;
-				}else{
-					temp+=cur;
+					temp = "" + cur;
+				} else {
+					temp += cur;
 				}
 			}
 		}
-		if(!temp.isEmpty())arr.add(temp);
+		if (!temp.isEmpty())
+			arr.add(temp);
 		return arr;
 	}
-	public ArrayList<Element> getElements(){
+
+	public ArrayList<Element> getElements() {
 		return rawElements;
 	}
-	public int getAtomicNumber(String str) {
+
+	public static int getAtomicNumber(String str) {
 		for (int i = 0; i < 92; i++) {
 			if (Element.chemTable[i].equals(str)) {
 				return i;
@@ -104,32 +181,45 @@ public class Chemical implements ChemicalCompoundable{
 
 	public String toString() {
 		String str = "";
-		for(int i = 0;i<parts.length;i++){
+		for (int i = 0; i < parts.length; i++) {
 			Object cur = parts[i];
-			str+=cur.toString();
+			str += cur.toString();
 		}
-		return amount>1?"("+str+")"+amount:str;
+		return amount > 1 ? "(" + str + ")" + amount : str;
 	}
-	public String toLatexString(){
+	public String debugString(){
+		System.out.println(parts.length);
+		String str = "{";
+		for (int i = 0; i < parts.length; i++) {
+			Object cur = parts[i];
+			if(cur instanceof Chemical){
+				str += ((Chemical)cur).debugString();
+			}else{
+				str += cur.toString();
+			}
+			str+=',';
+		}
+		str=str.substring(0, str.length()-1);
+		str+='}';
+		return str+":"+amount;
+	}
+
+	public String toLatexString() {
 		String str = "";
-		for(int i = 0;i<parts.length;i++){
-			str+=parts[i].toLatexString();
+		for (int i = 0; i < parts.length; i++) {
+			str += parts[i].toLatexString();
 		}
-		
-		return (amount>1?"("+str+")_{"+amount+"}":str);
+
+		return (amount > 1 ? "(" + str + ")_{" + amount + "}" : str);
 	}
+
 	@Override
-	public double getMolarMass(){
+	public double getMolarMass() {
 		return molarMass;
 	}
 	/*
-	public static double calcMolarMass(ArrayList<Element> elems){
-		double total = 0;
-		for(int i = 0;i<elems.size();i++){
-			Element cur = elems.get(i);
-			total+=cur.amount*Element.elementMass[cur.id-1];
-		}
-		return total;
-	}
-	*/
+	 * public static double calcMolarMass(ArrayList<Element> elems){ double
+	 * total = 0; for(int i = 0;i<elems.size();i++){ Element cur = elems.get(i);
+	 * total+=cur.amount*Element.elementMass[cur.id-1]; } return total; }
+	 */
 }
