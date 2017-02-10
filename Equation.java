e2ec8a6db7159907aea39446b81c6f97ad7be78b
NinjaDoggy <ninjadoggy1234@gmail.com>
2017-02-09 21:53:11 -0800

    Add files via upload

--- Equation.java
+++ Equation.java
@@ -1,71 +1,154 @@
 import java.util.*;
 
 public class Equation {
-	public ArrayList<Chemical> reactants;
-	public ArrayList<Chemical> products;
+	public ArrayList<ChemicalCompoundable> reactants;
+	public ArrayList<ChemicalCompoundable> products;
+	public int[] reactantAmounts;
+	public int[] productAmounts;
 	boolean balanced;
 
-	public Equation(ArrayList<Chemical> r, ArrayList<Chemical> p) {
+	public Equation(ArrayList<ChemicalCompoundable> r, ArrayList<ChemicalCompoundable> p) {
 		reactants = r;
 		products = p;
-		balanced = true;
+		reactantAmounts = new int[reactants.size()];
+		Arrays.fill(reactantAmounts, 1);
+		productAmounts = new int[products.size()];
+		Arrays.fill(productAmounts, 1);
+		balanced = false;
+	}
+
+	public Equation(String str) {
+		StringTokenizer st = new StringTokenizer(str);
+		boolean isProduct = false;
+		reactants = new ArrayList<ChemicalCompoundable>();
+		products = new ArrayList<ChemicalCompoundable>();
+		while (st.hasMoreTokens()) {
+			String token = st.nextToken();
+			if (token.equals("->")) {
+				isProduct = true;
+			} else if (!token.equals("+")) {
+				if (isProduct) {
+					products.add(new Chemical(token));
+				} else {
+					reactants.add(new Chemical(token));
+				}
+			}
+		}
+		reactantAmounts = new int[reactants.size()];
+		Arrays.fill(reactantAmounts, 1);
+		productAmounts = new int[products.size()];
+		Arrays.fill(productAmounts, 1);
+		balanced = false;
 	}
 
 	public void balance() {
+		balanced = true;
+		LinkedList<State> stack = new LinkedList();
+		stack.add(new State(reactantAmounts, productAmounts));
+		HashMap<String,Boolean> done = new HashMap<>();
 		while (true) {
-			ArrayList<Element> arr = new ArrayList<>();
-
-			for (int i = 0; i < reactants.size(); i++) {
-				ArrayList<Element> curElems = reactants.get(i).elems;
-				int multiple = reactants.get(i).amount;
-				for (int j = 0; j < curElems.size(); j++) {
-					Element curElem = curElems.get(j);
-					int index = indexOf(arr, curElem);
-					if (index == -1) {
-						arr.add(new Element(curElem.id, multiple * curElem.amount));
-					} else {
-						arr.get(index).amount += multiple * curElem.amount;
-					}
-				}
-			}
-			for (int i = 0; i < products.size(); i++) {
-				ArrayList<Element> curElems = products.get(i).elems;
-				int multiple = -products.get(i).amount;
-				for (int j = 0; j < curElems.size(); j++) {
-					Element curElem = curElems.get(j);
-					int index = indexOf(arr, curElem);
-					if (index == -1) {
-						arr.add(new Element(curElem.id, multiple * curElem.amount));
-					} else {
-						arr.get(index).amount += multiple * curElem.amount;
-						if (arr.get(index).amount == 0)
-							arr.remove(index);
-					}
-				}
-			}
-
-			if (arr.size() > 0) {
-				Element elemToChange = arr.get(0);
-				if (elemToChange.amount < 0) {
-					for (int i = 0; i < reactants.size(); i++) {
-						Chemical cur = reactants.get(i);
-						if (indexOf(cur.elems, elemToChange) != -1) {
-							reactants.get(i).amount++;
-							break;
-						}
-					}
-				} else{
-					for (int i = 0; i < products.size(); i++) {
-						Chemical cur = products.get(i);
-						if (indexOf(cur.elems, elemToChange) != -1) {
-							products.get(i).amount++;
-							break;
-						}
-					}
-				}
-			} else {
+			State curState = stack.remove();
+			
+			ArrayList<Element> offSets = curState.getStateOffSets(reactants, products);
+			if (offSets.size() == 0) {
+				reactantAmounts = curState.reactAmounts;
+				productAmounts = curState.prodAmounts;
 				break;
 			}
+			boolean[] incrementR = new boolean[reactants.size()];
+			boolean[] incrementP = new boolean[products.size()];
+			for (int i = 0; i < offSets.size(); i++) {
+				Element unBalancedElem = offSets.get(i);
+				if (unBalancedElem.amount > 0) {
+					// add product
+					for (int j = 0; j < products.size(); j++) {
+						if (incrementP[j])
+							continue;
+						if (products.get(j).copiesOfElement(unBalancedElem) > 0) {
+							incrementP[j] = true;
+						}
+					}
+				} else {
+					// add react
+					for (int j = 0; j < reactants.size(); j++) {
+						if (incrementR[j])
+							continue;
+						if (reactants.get(j).copiesOfElement(unBalancedElem) > 0) {
+							incrementR[j] = true;
+						}
+					}
+				}
+			}
+			for (int i = 0; i < incrementR.length; i++) {
+				if (incrementR[i]) {
+					int[] newAmounts = new int[incrementR.length];
+					System.arraycopy(curState.reactAmounts, 0, newAmounts, 0, incrementR.length);
+					newAmounts[i]++;
+					State newState = new State(newAmounts, curState.prodAmounts);
+					String str = newState.toString();
+					if (!done.containsKey(str)) {
+						stack.add(newState);
+						done.put(str,true);
+					}
+				}
+			}
+			for (int i = 0; i < incrementP.length; i++) {
+				int[] newAmounts = new int[incrementP.length];
+				System.arraycopy(curState.prodAmounts, 0, newAmounts, 0, incrementP.length);
+				newAmounts[i]++;
+				State newState = new State(curState.reactAmounts, newAmounts);
+				String str = newState.toString();
+				if (!done.containsKey(str)) {
+					stack.add(newState);
+					done.put(str,true);
+				}
+			}
+		}
+	}
+
+	private class State {
+		public int[] reactAmounts;
+		public int[] prodAmounts;
+
+		public State(int[] r, int[] p) {
+			reactAmounts = r;
+			prodAmounts = p;
+		}
+
+		public ArrayList<Element> getStateOffSets(ArrayList<ChemicalCompoundable> r,
+				ArrayList<ChemicalCompoundable> p) {
+			ArrayList<Element> arr = new ArrayList<Element>();
+			for (int i = 0; i < r.size(); i++) {
+				ArrayList<Element> curList = r.get(i).getElements();
+				for (int j = 0; j < curList.size(); j++) {
+					Element cur = curList.get(j);
+					int index = arr.indexOf(cur);
+					if (index == -1) {
+						arr.add(new Element(cur.id, reactAmounts[i] * cur.amount));
+					} else {
+						arr.get(i).amount += reactAmounts[i] * cur.amount;
+					}
+				}
+			}
+			for (int i = 0; i < p.size(); i++) {
+				ArrayList<Element> curList = p.get(i).getElements();
+				for (int j = 0; j < curList.size(); j++) {
+					Element cur = curList.get(j);
+					int index = arr.indexOf(cur);
+					arr.get(index).amount -= prodAmounts[i] * cur.amount;
+				}
+			}
+			for (int i = 0; i < arr.size(); i++) {
+				if (arr.get(i).amount == 0) {
+					arr.remove(i);
+					i--;
+				}
+			}
+			return arr;
+		}
+		@Override
+		public String toString(){
+			return Arrays.toString(reactAmounts)+Arrays.toString(prodAmounts);
 		}
 	}
 
@@ -78,18 +161,25 @@ public class Equation {
 
 	public String toLatexString() {
 		String str = "";
+		System.out.println("reactants: " + reactants + ", products:" + products);
 		for (int i = 0; i < reactants.size(); i++) {
-			str += reactants.get(i).toLatexStringEquation();
+			if (reactantAmounts[i] != 1) {
+				str += reactantAmounts[i];
+			}
+			str += reactants.get(i).toLatexString();
 			if (i < reactants.size() - 1) {
-				str += " + ";
+				str += "%20+%20";
 			} else {
-				str += " \\rightarrow ";
+				str += "%20\\rightarrow%20";
 			}
 		}
 		for (int i = 0; i < products.size(); i++) {
-			str += products.get(i).toLatexStringEquation();
+			if (productAmounts[i] != 1) {
+				str += productAmounts[i];
+			}
+			str += products.get(i).toLatexString();
 			if (i < products.size() - 1) {
-				str += " + ";
+				str += "%20+%20";
 			}
 		}
 		return str;
@@ -97,13 +187,13 @@ public class Equation {
 
 	public int amountOf(Chemical c) {
 		for (int i = 0; i < reactants.size(); i++) {
-			if (reactants.get(i).elems.equals(c.elems)) {
-				return reactants.get(i).amount;
+			if (reactants.get(i).getElements().equals(c.getElements())) {
+				return reactantAmounts[i];
 			}
 		}
 		for (int i = 0; i < products.size(); i++) {
-			if (products.get(i).elems.equals(c.elems)) {
-				return products.get(i).amount;
+			if (products.get(i).getElements().equals(c.getElements())) {
+				return productAmounts[i];
 			}
 		}
 		return 0;
